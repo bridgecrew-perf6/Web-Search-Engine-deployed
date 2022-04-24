2271ae705ca2b8cb2a15367d18e9a0e97d9ef29d
Hang <hz2447@nyu.edu>
2022-04-24 19:37:11 +0000

    Depolyed WSE

---
+++ query/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import os
+
+
+# Create your views here.
+def index(request):
+    context = {}
+    context['result'] = None
+    context['query'] = None
+    if 'fname' in request.GET and request.GET['fname']:
+        query = request.GET['fname']
+        context['query'] = query
+        print(query)
+        print(os.getcwd())
+        with open('/home/hangzhang/test/media/queryset.txt','a+') as fa:
+            fa.write(query.strip()+'\n')
+        tmp = query.strip().split()
+        if len(tmp) > 8:
+            tmp = tmp[:8]
+        query = ' '.join(tmp)
+        with open('/home/hangzhang/test/media/query.txt', 'w')as fw:
+            fw.write(query)
+        os.system("/home/hangzhang/test/client")
+        with open('/home/hangzhang/test/media/result.txt', 'r',encoding="utf-8")as fr:
+            result = fr.read()
+            result = result.replace('\n', '<br>')
+            context['result'] = result
+        os.system("rm result.txt")
+        with open('/home/hangzhang/test/media/result.txt', 'w')as fw:
+            fw.write("No result...")
+    return render(request, "index.html", context)
