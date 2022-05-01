69ac86fca9a601ddc6d28fb7e954dbcf4305a06b
Hang <hz2447@nyu.edu>
2022-05-01 21:34:41 +0000

    change the absolute path

--- query/views.py
+++ query/views.py
@@ -12,20 +12,20 @@ def index(request):
         context['query'] = query
         print(query)
         print(os.getcwd())
-        with open('/home/hangzhang/test/media/queryset.txt','a+') as fa:
+        with open('/home/hangzhang/Web-Search-Engine-deployed/media/queryset.txt','a+') as fa:
             fa.write(query.strip()+'\n')
         tmp = query.strip().split()
         if len(tmp) > 8:
             tmp = tmp[:8]
         query = ' '.join(tmp)
-        with open('/home/hangzhang/test/media/query.txt', 'w')as fw:
+        with open('/home/hangzhang/Web-Search-Engine-deployed/media/query.txt', 'w')as fw:
             fw.write(query)
-        os.system("/home/hangzhang/test/client")
-        with open('/home/hangzhang/test/media/result.txt', 'r',encoding="utf-8")as fr:
+        os.system("/home/hangzhang/Web-Search-Engine-deployed/client")
+        with open('/home/hangzhang/Web-Search-Engine-deployed/media/result.txt', 'r',encoding="utf-8")as fr:
             result = fr.read()
             result = result.replace('\n', '<br>')
             context['result'] = result
         os.system("rm result.txt")
-        with open('/home/hangzhang/test/media/result.txt', 'w')as fw:
+        with open('/home/hangzhang/Web-Search-Engine-deployed/media/result.txt', 'w')as fw:
             fw.write("No result...")
     return render(request, "index.html", context)
